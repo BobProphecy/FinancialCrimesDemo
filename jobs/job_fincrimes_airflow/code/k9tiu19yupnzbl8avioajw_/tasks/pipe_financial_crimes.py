a2f83df2fc489d0987b7d4877e78a2ec877eceb1
bobw@prophecy.io <bobw@prophecy.io>
2024-08-14 14:47:08 +0000

    Updates prophecy-libs dependencies for job_fin_crimes and job_fincrimes_airflow

--- jobs/job_fincrimes_airflow/code/k9tiu19yupnzbl8avioajw_/tasks/pipe_financial_crimes.py
+++ jobs/job_fincrimes_airflow/code/k9tiu19yupnzbl8avioajw_/tasks/pipe_financial_crimes.py
@@ -45,7 +45,7 @@ def pipe_financial_crimes(ti=None, params=None, **context):
             "entry_point": "main", 
             "parameters": ["-i", "default", "-O", "{}"]
           }, 
-          "libraries": [{"maven" : {"coordinates" : "io.prophecy:prophecy-libs_2.12:3.3.0-8.0.29"}},                          {"pypi" : {"package" : "prophecy-libs==1.9.9"}},                          {
+          "libraries": [{"maven" : {"coordinates" : "io.prophecy:prophecy-libs_2.12:3.3.0-8.0.31"}},                          {"pypi" : {"package" : "prophecy-libs==1.9.9"}},                          {
                            "whl": "dbfs:/FileStore/prophecy/artifacts/saas/app/__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__/pipeline/pipe_financial_crimes-1.0-py3-none-any.whl"
                          }]
         },
